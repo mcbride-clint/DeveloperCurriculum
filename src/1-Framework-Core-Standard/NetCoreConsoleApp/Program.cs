f0259b73c2beb633c64948c6449ff8f92688c570
mcbride-clint <cmm5559@gmail.com>
2021-06-16 15:26:16 -0400

    Create an example Framework and Core Console App

---
+++ src/1-Framework-Core-Standard/NetCoreConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using StandardLibrary;
+using System;
+
+namespace NetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal totalPrice = 12.99m;
+
+            var taxCalculator = new SalesTaxCalculator();
+
+            var salesTax = taxCalculator.CalculateTax(totalPrice);
+
+            Console.WriteLine($"Total Cost is $ {string.Format("{0:0.##}", salesTax + totalPrice)}");
+
+            Console.WriteLine("Press any key to exit");
+            _ = Console.ReadKey();
+        }
+    }
+}
---
+++ src/1-Framework-Core-Standard/StandardLibrary/SalesTaxCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StandardLibrary
+{
+    public class SalesTaxCalculator
+    {
+        public decimal CalculateTax(decimal totalPrice)
+        {
+            return totalPrice * 0.7m;
+        }
+    }
+}
