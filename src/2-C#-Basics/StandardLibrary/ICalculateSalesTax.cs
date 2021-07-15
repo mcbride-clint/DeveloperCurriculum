936ae4beff5ded7369ebc5d0153f40cc007e606c
mcbride-clint <cmm5559@gmail.com>
2021-07-15 10:29:37 -0400

    Add Comments to ICalculateSalesTax Interface

--- src/2-C#-Basics/StandardLibrary/ICalculateSalesTax.cs
+++ src/2-C#-Basics/StandardLibrary/ICalculateSalesTax.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace StandardLibrary
+﻿namespace StandardLibrary
 {
+    /// <summary>
+    /// Classes that Implement this will have a these function(s) to be able to Calculate Sales Tax
+    /// </summary>
     public interface ICalculateSalesTax
     {
+        /// <summary>
+        /// Calculate a sales tax given an item price
+        /// </summary>
+        /// <param name="itemPrice">Price of an Item</param>
+        /// <returns>decimal</returns>
         decimal CalculateTax(decimal itemPrice);
     }
 }
